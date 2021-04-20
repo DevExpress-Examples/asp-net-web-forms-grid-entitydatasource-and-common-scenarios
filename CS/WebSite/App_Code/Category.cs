3b59cb3b9def9fc27e29d02112f393517cd5cc61
CodeKarno <ekaterina.khlopova@devexpress.com>
2021-04-20 09:50:09 +0300

    updated to EF6, removed unnecessary assembly references

---
+++ CS/WebSite/App_Code/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.Spatial;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+    public partial class Category
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int CategoryID { get; set; }
+        [Required]
+        [StringLength(15)]
+        public string CategoryName { get; set; }
+        [Column(TypeName = "ntext")]
+        public string Description { get; set; }
+        [Column(TypeName = "image")]
+        public byte[] Picture { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
