2b852647e2fed207ec00391c8115d6d15add92c1
CodeKarno <ekaterina.khlopova@devexpress.com>
2021-04-20 09:51:21 +0300

    automatic CRUD page removed, because EF6 does not support EntityDataSource without a workaround with referencing EF5

--- CS/WebSite/AutomaticEditingCapabilities.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using DevExpress.Web;
-
-public partial class AutomaticEditingCapabilities : System.Web.UI.Page {
-    protected void grid_DataBinding(object sender, EventArgs e) {
-        (sender as ASPxGridView).ForceDataRowType(typeof(NorthwindModel.Categories));
-    }
-}
